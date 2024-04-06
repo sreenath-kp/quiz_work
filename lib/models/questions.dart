924534f3ff7c0494ab89744c0f29ae4a95384fc1
Sreenath <108588566+sreenath-kp@users.noreply.github.com>
2024-04-07 00:05:17 +0530

    question model

---
+++ lib/models/questions.dart
@@ -0,0 +1,10 @@
+// We need to fetch the questions from the API
+// create a question model
+
+class Question {
+  final String question;
+  final List<String> answers;
+  const Question(this.question, this.answers);
+}
+
+
